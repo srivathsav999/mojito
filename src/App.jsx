f900baf92ccad9f16496fd2170fdb35881911f49
Srivathsav-Kyatham <2023.srivathsavk@isu.ac.in>
2025-07-06 18:11:59 +0530

    rename

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <div className="flex-center h-[100vh]">
-      <h1 className="">Welcome to My React GSAP App!!</h1>
+      <h1 className="">Welcome to My GSAP App!!</h1>
     </div>
   );
 };
